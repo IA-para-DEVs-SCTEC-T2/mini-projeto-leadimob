48251dca7777fbc6c2085d2471580bc5f0847b11
figtisolucoes <infra@figti.com.br>
2026-04-28 20:41:07 -0300

    feat: add função de checagem de duplicadas.

---
+++ lab.js
@@ -0,0 +1,12 @@
+function hashDuplicate(nums) {
+    const seen = new Set()
+
+    for (const num of nums) {
+        if (seen.has(num)) {
+            return num
+        }
+        seen.add(num)
+    }
+
+    return -1
+}
